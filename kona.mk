b2a3faa0955c2e62f052d982175dd11074d86fdc
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    sm8250-common: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55

--- kona.mk
+++ kona.mk
@@ -305,6 +305,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# LMKD
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
