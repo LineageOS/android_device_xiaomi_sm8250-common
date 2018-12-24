bc4e9fc89940877b78e82819b9f85d07ed8271e2
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    sm8250-common: Support Alipay and WeChat fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
    
    Change-Id: Ied17d6456561bb399e4dd4a868d57a48d2d51db2

--- kona.mk
+++ kona.mk
@@ -235,6 +235,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
