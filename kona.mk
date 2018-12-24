d03d8461e799abd67469eaf19cefac8666ffea5d
notsyncing <song.fc@gmail.com>
2018-12-24 21:37:03 +0800

    sm8250-common: audio: Use USB audio policy configuration from AOSP
     * This can greatly improve audio quality when an external
       USB DAC is connected.
    
    Change-Id: I4eeaadc9e3dd26fb54dbfa5a1acf3599b3e6b16b

--- kona.mk
+++ kona.mk
@@ -164,7 +164,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
