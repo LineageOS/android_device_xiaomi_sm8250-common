725e027d3db7fdf2a35b1d288232fd45673ee405
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    sm8250-common: releasetools: Ship and update vbmeta image
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972

--- releasetools.py
+++ releasetools.py
@@ -37,4 +37,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   return
