5deb4ee494f15d5909cc4d829183579db4d9a646
TheScarastic <warabhishek@gmail.com>
2020-05-24 19:54:01 +0200

    sm8250-common: fod: Prepare for in kernel dim and hbm
    Change-Id: Id92963ff70a379e02897596b6719a0f3dab37ab2

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -18,8 +18,6 @@
 
 #define TOUCH_FOD_ENABLE 10
 
-#define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
-
 namespace vendor {
 namespace lineage {
 namespace biometrics {
@@ -27,15 +25,6 @@ namespace fingerprint {
 namespace inscreen {
 namespace V1_0 {
 namespace implementation {
-
-template <typename T>
-static T get(const std::string& path, const T& def) {
-    std::ifstream file(path);
-    T result;
-
-    file >> result;
-    return file.fail() ? def : result;
-}
 
 FingerprintInscreen::FingerprintInscreen() {
     mTouchFeatureService = ITouchFeature::getService();
@@ -86,21 +75,8 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
-    float alpha;
-    int realBrightness = get(BRIGHTNESS_PATH, 0);
-
-    if (realBrightness >= 500) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.485);
-    } else if (realBrightness >= 250) {
-        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.530);
-    } else if (realBrightness > 60) {
-         alpha = 1.0 - pow(realBrightness / 1680.0, 0.525);
-    } else {
-        alpha = 1.0 - pow(realBrightness / 1680.0, 0.475);
-    }
-
-    return 255 * alpha;
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
+    return 0;
 }
 
 Return<bool> FingerprintInscreen::shouldBoostBrightness() {
