7361aca17c8bea05de0b5747f2a9dfbafbba1fd4
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    sm8250-common: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- kona.mk
+++ kona.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
