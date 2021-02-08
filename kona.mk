5cdf1588ce1dfb23f1b31c85b6537818cbb193af
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:36:57 +0100

    sm8250-common: Setup namespace
    Change-Id: I97172190de6bcf572021b5a252a1dc587e1a3fc4

--- kona.mk
+++ kona.mk
@@ -13,5 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
