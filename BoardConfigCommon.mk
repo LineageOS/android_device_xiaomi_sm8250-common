4fd92fb723b532c2a224eec35eb7387ce930355b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 08:50:41 +0100

    sm8250-common: Add platform flags
    Change-Id: I9172d913cd1500a63f5f9da2e9709a77dfc718de

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,5 +61,9 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := kona
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
