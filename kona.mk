f49e4db3ae7635c9fa504664a968d108a5a0725d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:37:15 +0100

    sm8250-common: Build fastbootd
    Change-Id: I7d23da657e7c6220c9cc3f80734884c204f5212a

--- kona.mk
+++ kona.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
