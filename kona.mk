07ea25abb86c176ec35b25601a43c0558d70429a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 14:48:21 +0100

    sm8250-common: Import wifi configs
    * From lmi-user 11 RKQ1.200826.002 V12.2.4.0.RJKMIXM release-keys
    
    Change-Id: I363ddda7a99bd261930300dec147f2b86d909727

--- kona.mk
+++ kona.mk
@@ -400,6 +400,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/qca6390/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6390/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
