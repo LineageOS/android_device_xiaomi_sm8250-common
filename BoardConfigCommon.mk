b11d777ae2064869e676029c2844c313265c9f8a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 08:37:49 +0100

    sm8250-common: Add arch and bootloader flags
    Change-Id: I578f01a20d604fd8c936cffbeed83b967906f638

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,24 @@
 
 COMMON_PATH := device/xiaomi/sm8250-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := kona
+TARGET_NO_BOOTLOADER := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
