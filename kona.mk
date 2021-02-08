5accfd3515ae3c5032378283cd3c8235283fe42d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:24:01 +0100

    sm8250-common: Don't build full super image
    Change-Id: I40ebb9c756c3546ef6f3de922a24c0660382932a

--- kona.mk
+++ kona.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
