748d1110ea87f798a6a6d92b672935a4c95cf165
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    sm8250-common: Switch to source built vendor.qti.hardware.perf
    * https://review.lineageos.org/c/LineageOS/android_vendor_qcom_opensource_interfaces/+/303909.
    
    Change-Id: I1dc96e4a51c4c81dd847c59a58ec670d89054f64

--- kona.mk
+++ kona.mk
@@ -295,6 +295,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
