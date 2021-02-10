23f348c854766becd8a2ffab43b496058ec743df
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:08:13 +0100

    sm8250-common: Build sensors HIDL
    Change-Id: I6986bce0baeb529afd9e9796360faa9380c5b054

--- kona.mk
+++ kona.mk
@@ -194,6 +194,12 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
