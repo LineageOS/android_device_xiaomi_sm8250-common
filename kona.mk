442cf4a5e4564323f5abd76e67482b9325d42c95
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:10:39 +0100

    sm8250-common: Build WFD libs
    Change-Id: I859cd3ff8e1729323f2c72df4feca55559262878

--- kona.mk
+++ kona.mk
@@ -237,5 +237,13 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
