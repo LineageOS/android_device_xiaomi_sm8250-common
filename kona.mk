06b616eaa74850d07b31af9fff1a55479687608c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:10:19 +0100

    sm8250-common: Build Wi-Fi stack
    Change-Id: I2e99bb95cbcdb2fdb652e20bb1e9e55b53cab2eb

--- kona.mk
+++ kona.mk
@@ -225,5 +225,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwpa_client \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
