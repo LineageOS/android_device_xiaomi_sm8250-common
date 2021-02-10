37349e222b7fbfe441532f19506e28aef71c90f2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:58:07 +0100

    sm8250-common: Build atrace service
    Change-Id: I89b2e6c308fb4ae63680ef25b56bc20a41a2c2a4

--- kona.mk
+++ kona.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
