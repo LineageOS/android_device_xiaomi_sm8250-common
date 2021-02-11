acb43a1ddeae2732142fd28cb90879147681657d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-11 11:02:49 +0100

    sm8250-common: Initial lineage-sdk overlays
    * Define hardware keys and add fastbootd to advanced reboot
    
    Change-Id: I9d2728eeeef5c72488824670d2a4c2bde5db7d51

--- kona.mk
+++ kona.mk
@@ -9,7 +9,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
