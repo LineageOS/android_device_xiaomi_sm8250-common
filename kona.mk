5ee669c101c7465134f3a4fb2eb491ad0ef31f14
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-17 17:21:17 +0100

    sm8250-common: Build QTI thermal HIDL
    Change-Id: Id8d468198cef11a67cd40a93a3c50de7185bec43

--- kona.mk
+++ kona.mk
@@ -410,6 +410,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
