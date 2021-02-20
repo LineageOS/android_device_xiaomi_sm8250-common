5f1b8d41b58209d1b714c939a7f65c8f7448e69b
Bruno Martins <bgcngm@gmail.com>
2021-02-20 01:25:08 +0100

    sm8250-common: Import power AIDL extension for DT2W
    This is a squash of the following commits:
    
    	From: Bruno Martins <bgcngm@gmail.com>
    	Date: Fri, 31 Jan 2020 23:10:19 +0000
    	Subject: sm8250-common: power: Scan input devices for eligible DT2W node
    
    		It is no longer a surprise for anyone to see Xiaomi shipping the same
    		device with different displays and different displays usually mean
    		different touchscreen drivers.
    
    		The current double tap handling in the power HAL only takes into
    		account one specific node, meant to be set in the device tree.
    		However, if the touchscreen was registered as a different input device
    		it would cause the feature to be broken. Address the issue by iterating
    		through the input devices in order to find one supported touchscreen
    		input to be used.
    
    		Co-authored-by: LuK1337 <priv.luk@gmail.com>
    		Change-Id: I2f7fb4a8b0c9cd1a16d6c2b93602d285a191f170
    
    	From: Bruno Martins <bgcngm@gmail.com>
    	Date: Fri, 9 Oct 2020 12:19:37 +0100
    	Subject: sm8250-common: power: Adapt extension to new aidl power HAL
    
    		Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9
    
    Change-Id: If30eaca900f0a046f7740c681e8707b5dce8e1a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
