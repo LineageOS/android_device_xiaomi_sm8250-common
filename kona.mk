380b727cc4c5f4b3440bcd3121e2089292959741
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-21 16:46:06 +0100

    sm8250-common: Build android.hidl.base@1.0
    Change-Id: I5457ebbadd95e4c43af4f4dd2e1a1413a241d865

--- kona.mk
+++ kona.mk
@@ -228,6 +228,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
