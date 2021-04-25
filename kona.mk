63500bb5013bf460a08743151b82b51de601c3a0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-25 03:19:44 +0200

    sm8250-common: Import excluded-input-devices.xml
    * From lmi-user 11 RKQ1.200826.002 V12.2.6.0.RJKMIXM release-keys
    
    Change-Id: I9ec50ed82b07ec5936e54d8cd021985b0c1b9486

--- kona.mk
+++ kona.mk
@@ -397,6 +397,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
