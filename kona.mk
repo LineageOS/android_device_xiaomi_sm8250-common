e46be1397259682bdd1b4bfd5e74c4e0dbcf8c07
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-17 14:58:15 +0200

    sm8250-common: Build default Trust HIDL
    Change-Id: Icfecbd97df7adccc51406ae667fbc3cb10865a5a

--- kona.mk
+++ kona.mk
@@ -382,6 +382,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
