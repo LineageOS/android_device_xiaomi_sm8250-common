9db0864cb761a8b831cead05b4b8958b99a3afa7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 00:30:05 +0200

    sm8250-common: Add power AIDL DT2W support via sysfs node
    Change-Id: Idcff758444a5c89ba20f2ae804a407ee78c445cb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
