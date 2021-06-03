8560fd18937a7d8cad3ea7acdbd7fddca41ca374
Albert I <kras@raphielgang.org>
2021-06-03 20:15:01 +0800

    sm8250-common: Add support for virtual A/B configuration
    * fstab_AB.qcom and partition size values are from
      alioth-user 11 RKQ1.200826.002 V12.5.1.0.RKHMIXM release-keys
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I3f6d26bbfd6c8f72c0f5621f83cddb2b4f69b1a3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -133,7 +133,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
+LOCAL_SRC_FILES    := etc/fstab_AB.qcom
+else
 LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 include $(BUILD_PREBUILT)
@@ -143,8 +147,13 @@ LOCAL_MODULE       := fstab.qcom_ramdisk
 LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
+LOCAL_SRC_FILES    := etc/fstab_AB.qcom
+LOCAL_MODULE_PATH  := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
+else
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+endif
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
