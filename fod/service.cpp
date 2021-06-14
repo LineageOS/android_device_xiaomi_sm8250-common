c6dd3170c9964e73fcc4912b1d328ecf2695a752
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-14 16:04:35 +0200

    sm8250-common: fod: Bump to 1.1
    Change-Id: Ib24f1bd4f1f68c1c5478a4cf01f5c43aca1ca6c3

--- fod/service.cpp
+++ fod/service.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_kona"
+#define LOG_TAG "vendor.lineage.biometrics.fingerprint.inscreen@1.1-service.xiaomi_kona"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
@@ -14,8 +14,8 @@
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreen;
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::implementation::FingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::IFingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::implementation::FingerprintInscreen;
 
 using android::OK;
 using android::status_t;
