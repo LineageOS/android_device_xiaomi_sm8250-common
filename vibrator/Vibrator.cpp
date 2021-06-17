0ad1d016d7f25292e440a26627453b396a4abf11
Ramii Ahmed <ramy@ahmedramy.com>
2021-06-17 08:45:15 +0000

    sm8250-common: vibrator: remove QTI haptic device detection
    this seems to break our awinic vibration motor, and we favor qti's vibrator hal over aosp's for it's effects, prevent the HAL from breaking due to our vibrator not being a "QTI" vibrator.
    
    Change-Id: I7cb56a0fd9bdd58d90b1a65e38def84704584155

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -53,7 +53,6 @@ namespace vibrator {
 #define LIGHT_MAGNITUDE         0x3fff
 #define INVALID_VALUE           -1
 #define CUSTOM_DATA_LEN         3
-#define NAME_BUF_SIZE           32
 
 #define MSM_CPU_LAHAINA         415
 #define APQ_CPU_LAHAINA         439
@@ -74,7 +73,6 @@ InputFFDevice::InputFFDevice()
     uint8_t ffBitmask[FF_CNT / 8];
     char devicename[PATH_MAX];
     const char *INPUT_DIR = "/dev/input/";
-    char name[NAME_BUF_SIZE];
     int fd, ret;
     int soc = property_get_int32("ro.vendor.qti.soc_id", -1);
 
@@ -106,20 +104,6 @@ InputFFDevice::InputFFDevice()
             continue;
         }
 
-        ret = TEMP_FAILURE_RETRY(ioctl(fd, EVIOCGNAME(sizeof(name)), name));
-        if (ret == -1) {
-            ALOGE("get input device name %s failed, errno = %d\n", devicename, errno);
-            close(fd);
-            continue;
-        }
-
-        if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")) {
-            ALOGD("not a qcom/qti haptics device\n");
-            close(fd);
-            continue;
-        }
-
-        ALOGI("%s is detected at %s\n", name, devicename);
         ret = TEMP_FAILURE_RETRY(ioctl(fd, EVIOCGBIT(EV_FF, sizeof(ffBitmask)), ffBitmask));
         if (ret == -1) {
             ALOGE("ioctl failed, errno = %d", errno);
