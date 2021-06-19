e5f7c5e73c023c7aeddb9885474a084395dec0fb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:46:02 +0200

    sm8250-common: vibrator: Rebrand to Xiaomi SM8250
    Change-Id: Icd0ec7c857574caec085751253fafa39be065d0d

--- vibrator/service.cpp
+++ vibrator/service.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.hardware.vibrator.service"
+#define LOG_TAG "vendor.qti.hardware.vibrator.service.xiaomi_kona"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
