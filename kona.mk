edc09e882f138e45974875e37481d5a69e888571
Albert I <kras@raphielgang.org>
2021-06-21 23:55:13 +0800

    sm8250-common: Set Bluetooth MAC address using persist property
    The idea is to allow us to not depend on stock QTI Bluetooth HAL, as MAC
    addresses fetched from NVRAM by nv_mac script will be saved as hex-encoded
    files. We can decode back saved files to then the Bluetooth one be set using
    persist property so it can be read by Bluetooth HAL.
    
    This is loosely based on similar techniques used on Mi 9 and ZenFone Max Pro M2.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I74d07c3c3125a04962c37fe8bfcc8385d1fd3398

--- kona.mk
+++ kona.mk
@@ -355,6 +355,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.mdm.sh \
+    init.mi.btmac.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
