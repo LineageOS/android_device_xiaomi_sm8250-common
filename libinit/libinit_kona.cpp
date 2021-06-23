d2ad0e272c64d3973ae97ba71b834ef18594b146
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:34:32 +0200

    sm8250-common: Initial variant handler libinit
    * This libinit has been made to commonize device variants props handling
    
    Change-Id: Iab68ff451ab1d6e861fb4cda4ef07fad3123ecde

---
+++ libinit/libinit_kona.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <libinit_kona.h>
+
+using android::base::GetProperty;
+
+void search_variant(const variant_info_t variants[]) {
+    std::string sku = GetProperty("ro.boot.product.hardware.sku", "");
+
+    for (const auto& variant : variants) {
+        if (sku.find(variant.sku_value) != std::string::npos) {
+            set_variant_props(variant);
+            break;
+        }
+    }
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("model", variant.model, true);
+    set_ro_build_prop("fingerprint", variant.build_fingerprint);
+    set_ro_build_prop("description", variant.build_description);
+}
+
+void property_override(char const prop[], char const value[], bool add = true) {
+    auto pi = (prop_info *) __system_property_find(prop);
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+std::vector<std::string> ro_props_default_source_order = {
+    "bootimage.",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "",
+};
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false) {
+    std::string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+}
