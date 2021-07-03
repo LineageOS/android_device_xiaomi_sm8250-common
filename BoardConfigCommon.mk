f78f94a59152ff1fef76ab894e22e6f26f2ee50b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-03 11:47:31 +0200

    sm8250-common: Move NFC manifest entries to nfc SKU
    Change-Id: I2d83a4551551f370c1ecffb73502c5bcec0bd5a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,8 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+ODM_MANIFEST_SKUS += nfc
+ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
 
 # Kernel
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
