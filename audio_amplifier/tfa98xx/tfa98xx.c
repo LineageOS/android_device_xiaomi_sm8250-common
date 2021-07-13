4cc5bbce3e64aa1d1ba0b813608ac2b8d90ed8d4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-13 02:38:28 +0200

    sm8250-common: audio_amplifier: tfa98xx: Extract in_snd_device value
    * From stock lmi audio HAL
    
    Change-Id: I001c3101867e69f93a33d8fb25335993b587fb3c

--- audio_amplifier/tfa98xx/tfa98xx.c
+++ audio_amplifier/tfa98xx/tfa98xx.c
@@ -79,7 +79,7 @@ int tfa98xx_start_feedback(void* adev, uint32_t snd_device) {
     }
     tfa_dev->usecase_tx->id = USECASE_AUDIO_SPKR_CALIB_TX;
     tfa_dev->usecase_tx->type = PCM_CAPTURE;
-    tfa_dev->usecase_tx->in_snd_device = SND_DEVICE_IN_CAPTURE_VI_FEEDBACK;
+    tfa_dev->usecase_tx->in_snd_device = 232;
     list_init(&tfa_dev->usecase_tx->device_list);
 
     list_add_tail(&tfa_dev->adev->usecase_list, &tfa_dev->usecase_tx->list);
