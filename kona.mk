8496e0ef53f5474eff46e7f8b542b31cebf5f030
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-22 16:32:58 +0200

    sm8250-common: Kill touchfeature
    Change-Id: Iea8dbe1a69906627ffdabca70ecea624435fc53e

--- kona.mk
+++ kona.mk
@@ -210,8 +210,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
 
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
-    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 # Health
 PRODUCT_PACKAGES += \
