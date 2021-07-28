f7ecbf4dbd9ec5452146329cb83aebb01df7eedc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-28 17:32:04 +0200

    sm8250-common: libinit: Add a function to set Dalvik heap info props
    * Leaving <=2GB props just because it doesn't hurt
    
    Change-Id: I5236e170b4283271da68f009ffe51f5e5c5d7565

--- libinit/libinit_kona.cpp
+++ libinit/libinit_kona.cpp
@@ -7,6 +7,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include <libinit_kona.h>
 
@@ -14,6 +15,42 @@ using android::base::GetProperty;
 
 #define HWC_PROP "ro.boot.hwc"
 #define SKU_PROP "ro.boot.product.hardware.sku"
+
+#define HEAPSTARTSIZE_PROP "dalvik.vm.heapstartsize"
+#define HEAPGROWTHLIMIT_PROP "dalvik.vm.heapgrowthlimit"
+#define HEAPSIZE_PROP "dalvik.vm.heapsize"
+#define HEAPTARGETUTILIZATION_PROP "dalvik.vm.heaptargetutilization"
+#define HEAPMINFREE_PROP "dalvik.vm.heapminfree"
+#define HEAPMAXFREE_PROP "dalvik.vm.heapmaxfree"
+
+#define TO_GB(b) (b * 1024ull * 1024 * 1024)
+
+static const dalvik_heap_info_t dalvik_heap_info_6144 {
+    .heapstartsize = "16m",
+    .heapgrowthlimit = "256m",
+    .heapsize = "512m",
+    .heaptargetutilization = "0.5",
+    .heapminfree = "8m",
+    .heapmaxfree = "32m",
+};
+
+static const dalvik_heap_info_t dalvik_heap_info_4096 {
+    .heapstartsize = "8m",
+    .heapgrowthlimit = "256m",
+    .heapsize = "512m",
+    .heaptargetutilization = "0.6",
+    .heapminfree = "8m",
+    .heapmaxfree = "16m",
+};
+
+static const dalvik_heap_info_t dalvik_heap_info_2048 {
+    .heapstartsize = "8m",
+    .heapgrowthlimit = "192m",
+    .heapsize = "512m",
+    .heaptargetutilization = "0.75",
+    .heapminfree = "512k",
+    .heapmaxfree = "8m",
+};
 
 void search_variant(const std::vector<variant_info_t> variants) {
     std::string hwc_value = GetProperty(HWC_PROP, "");
@@ -40,6 +77,27 @@ void set_variant_props(const variant_info_t variant) {
 
     if (variant.nfc)
         property_override(SKU_PROP, "nfc");
+}
+
+void set_dalvik_heap() {
+    struct sysinfo sys;
+    const dalvik_heap_info_t *dhi;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > TO_GB(5))
+        dhi = &dalvik_heap_info_6144;
+    else if (sys.totalram > TO_GB(3))
+        dhi = &dalvik_heap_info_4096;
+    else
+        dhi = &dalvik_heap_info_2048;
+
+    property_override(HEAPSTARTSIZE_PROP, dhi->heapstartsize);
+    property_override(HEAPGROWTHLIMIT_PROP, dhi->heapgrowthlimit);
+    property_override(HEAPSIZE_PROP, dhi->heapsize);
+    property_override(HEAPTARGETUTILIZATION_PROP, dhi->heaptargetutilization);
+    property_override(HEAPMINFREE_PROP, dhi->heapminfree);
+    property_override(HEAPMAXFREE_PROP, dhi->heapmaxfree);
 }
 
 void property_override(std::string prop, std::string value, bool add) {
