9d7d3b0396cc16d4f25ae1e893edff83fcda12f5
BeYkeRYkt <beykerykt@gmail.com>
2021-09-24 21:19:49 +0900

    sm8250-common: Import Pixel power stats HAL
    * Fixes: Unable to load Power Hal or power.stats HAL
    
    Change-Id: I75de3aa0cc5cc202d1b45926adede7b7bb11c437

--- kona.mk
+++ kona.mk
@@ -332,7 +332,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.xiaomi_sm8250
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -388,7 +389,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
