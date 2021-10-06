524f121c684e4f9ab8153aaffd0464c00d84390e
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:22:58 +0000

    sm8250-common: biometrics/fod: Uprev to 2.3
     * [SebaUbuntu]: Move fingerprint HIDL to 2.3
     * Remove Lineage FOD HIDL
     * Rename fodextension to udfpsextension
    
    Change-Id: I292bbb9509de89eb9031daebab3cb5b2e5411d21

--- fod/UdfpsExtension.cpp
+++ fod/UdfpsExtension.cpp
@@ -4,16 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <compositionengine/FodExtension.h>
+#include <compositionengine/UdfpsExtension.h>
 #include <drm/sde_drm.h>
 
-uint32_t getFodZOrder(uint32_t z, bool touched) {
+uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
         z |= FOD_PRESSED_LAYER_ZORDER;
     }
     return z;
 }
 
-uint64_t getFodUsageBits(uint64_t usageBits, bool) {
+uint64_t getUdfpsUsageBits(uint64_t usageBits, bool) {
     return usageBits;
 }
