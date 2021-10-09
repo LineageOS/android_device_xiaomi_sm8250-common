300fa0da7fbb28c3882426d6492f160e262ee734
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-09 15:21:32 +0200

    sm8250-common: Build all extra VNDK versions
    * Basically make system a GSI, but this should avoid us the headache made by VNDK libs not being built based on proprietary interfaces
    
    Change-Id: I1ca876b5d2a82d41af76f4c327b89a181bb3d6bd

--- kona.mk
+++ kona.mk
@@ -480,6 +480,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
