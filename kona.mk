586f12f12455b0888942a505dd3b6310b4c39f27
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 00:46:51 +0200

    sm8250-common: Uprev clearkey DRM HIDL to 1.4
    Change-Id: Id0895fee70495913f57bc8408ea433660467a7c7

--- kona.mk
+++ kona.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
