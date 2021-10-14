bf5de77aaa9bef809ea9568d74eb400cd498640d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-14 19:15:50 +0200

    sm8250-common: Move to AOSP NXP NFC HAL
    * NXP added snxxx support in AOSP, \o/
    
    Change-Id: Id1b9cdbe1f29d1ff61af8182ae2f8370e5210426
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- kona.mk
+++ kona.mk
@@ -357,23 +357,14 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/opensource/sn100x
-
 PRODUCT_PACKAGES += \
+    android.hardware.nfc_snxxx@1.2-service \
     com.android.nfc_extras \
     libchrome.vendor \
-    com.nxp.nfc.nq \
-    libnqnfc_nci_jni \
-    nfc_nci.nqx.default.hw \
-    NQNfcNci \
+    NfcNci \
     SecureElement \
     Tag \
-    android.hardware.secure_element@1.2.vendor \
-    se_nq_extn_client \
-    ls_nq_client \
-    jcos_nq_client \
-    vendor.nxp.hardware.nfc@2.0-service
+    android.hardware.secure_element@1.2.vendor
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
