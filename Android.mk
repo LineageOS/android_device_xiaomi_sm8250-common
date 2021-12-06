8f5be99f5cab978467e30539776e639c5ead61b2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-06 01:22:32 +0100

    sm8250-common: Add support for elish and enuma
    * Xiaomi Pad 5 Pro Wi-Fi and Xiaomi Pad 5 Pro 5G
    
    Change-Id: Ic741fa12553ea4e063ca101c51ce62f9656682ca

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollon cas cmi lmi thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth apollon cas cmi elish enuma lmi thyme umi,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
