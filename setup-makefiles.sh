e05bac0dad6c5e5a1c1d2803ad738ff4995b96e0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-06 01:25:18 +0100

    sm8250-common: mikona?
    * Unified A-only devices and unified VAB devices
    
    Change-Id: I387d8984b94c505bdbb7c6e51b0c60572018aab9

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "alioth apollon cas cmi elish enuma lmi thyme umi"
+write_headers "mikona_a mikona_vab"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
