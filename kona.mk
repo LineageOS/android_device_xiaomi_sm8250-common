ffb913626767a8456631b9672347cdd0e8250681
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-06 18:23:14 +0100

    sm8250-common: Build our multihal sensors HAL
    Change-Id: I3ac85db9fda41e2be427184c38f8b0cbb190dd46

--- kona.mk
+++ kona.mk
@@ -389,7 +389,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.kona
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
