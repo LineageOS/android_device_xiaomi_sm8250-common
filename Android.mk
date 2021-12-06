5cebead97e95a2e6de12d75b6fe0602f3f73f5dc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-06 01:25:18 +0100

    sm8250-common: mikona?
    * Unified A-only devices and unified VAB devices
    
    Change-Id: I387d8984b94c505bdbb7c6e51b0c60572018aab9

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollon cas cmi elish enuma lmi thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter mikona_a mikona_vab,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
