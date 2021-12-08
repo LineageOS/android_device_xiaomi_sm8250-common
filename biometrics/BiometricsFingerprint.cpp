bf25f0cb80fc432c2794772b1a8e4616bdc3c890
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 00:34:05 +0100

    sm8250-common: biometrics: Add support for fpc_fod and goodix
    * fpc_fod is used by umi
    * goodix is used by elish/enuma
    
    Change-Id: Iff8f996055ac82d833d9e7c3613720c3274b6800

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -39,6 +39,8 @@ static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 // List of fingerprint HALs
 static const char *kHALClasses[] = {
     "fpc",
+    "fpc_fod",
+    "goodix",
     "goodix_fod",
     "goodix_fod6",
 };
