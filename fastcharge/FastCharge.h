3822c534eb161413da3d1d4da7058ede767c6ae5
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:37:05 +0000

    sm8250-common: fastcharge: Use pragma once
    Change-Id: I9a9e193a6f66f045f41394d059af8b71778e8d72

--- fastcharge/FastCharge.h
+++ fastcharge/FastCharge.h
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_FASTCHARGE_V1_0_FASTCHARGE_H
-#define VENDOR_LINEAGE_FASTCHARGE_V1_0_FASTCHARGE_H
+#pragma once
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
@@ -49,5 +48,3 @@ struct FastCharge : public IFastCharge {
 } // namespace fastcharge
 } // namespace lineage
 } // namespace vendor
-
-#endif // VENDOR_LINEAGE_FASTCHARGE_V1_0_FASTCHARGE_H
