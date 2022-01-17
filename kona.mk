6af35c265a0f04bb2c2b2e847a096151cf87d5aa
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-17 16:47:53 +0100

    sm8250-common: Resurrect touchfeature
    Change-Id: Ie920dd65965fdb4807bba9b42388f376b3f34c12
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- kona.mk
+++ kona.mk
@@ -468,6 +468,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touchfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
