738fe59963ed903acb240182c61780c842436a62
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    sm8250-common: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- kona.mk
+++ kona.mk
@@ -207,7 +207,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
