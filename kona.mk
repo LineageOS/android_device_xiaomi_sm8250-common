73c5c16e020c53d71401655876ef23517b9aad2c
LuK1337 <priv.luk@gmail.com>
2022-03-16 01:30:14 +0100

    sm8250-common: Remove trust HAL from product packages
    USB 1.3 HAL now provides the same functionality and even more.
    
    Change-Id: Idda908a90d3b935e1065eb41abec4babeefc4fd0

--- kona.mk
+++ kona.mk
@@ -472,10 +472,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
