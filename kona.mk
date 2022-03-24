89e4798031351733e4b88b3a4b44683ede09627f
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    sm8250-common: Inherit launch_with_vendor_ramdisk for vAB
    - alioth has vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- kona.mk
+++ kona.mk
@@ -82,7 +82,7 @@ endif
 # A/B
 ifeq ($(TARGET_IS_VAB),true)
 # Inherit virtual_ab_ota product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
