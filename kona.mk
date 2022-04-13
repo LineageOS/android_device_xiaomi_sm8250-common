f02c54df6b8da5dedbe5409b274e472202c297f7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    sm8250-common: Move to common IfaaService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2

--- kona.mk
+++ kona.mk
@@ -282,10 +282,7 @@ PRODUCT_COPY_FILES += \
 
 # IFAA manager
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    IfaaService
 
 # IPACM
 PRODUCT_PACKAGES += \
