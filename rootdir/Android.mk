45aef7ce2c3b563e3363d9d6dabf8d88cb0cdf8f
althafvly <althafvly@gmail.com>
2022-04-19 14:14:05 +0530

    sm8250-common: rootdir: Decommonize fstab.qcom
    Change-Id: I29abc74d461480e661caa97ba7b72bb17e997940

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,20 +15,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom_ramdisk
-LOCAL_MODULE_STEM  := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
-LOCAL_SRC_FILES    := etc/fstab_AB.qcom
-LOCAL_MODULE_PATH  := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
-else
-LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
-endif
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
