275c306bdf17bbfd2406dfa5ba50a99610704fa3
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    sm8250-common: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- kona.mk
+++ kona.mk
@@ -320,6 +320,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
