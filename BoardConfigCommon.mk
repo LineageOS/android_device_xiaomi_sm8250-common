34b2c7d31f0d9bb1eb2932eb07708f642a7bebfe
althafvly <althafvly@gmail.com>
2022-05-13 19:32:52 +0530

    sm8250-common: Update blobs to V13.0.3.0.SKHMIXM
    Change-Id: I5e0b855b11990a1e96ad8c7523b62a2ae741b571

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -210,7 +210,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-02-01
+VENDOR_SECURITY_PATCH := 2022-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
