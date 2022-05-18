7c2f8544f17756680fd9055f5553716251bc0a4e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:43:16 +0200

    sm8250-common: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building  vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- kona.mk
+++ kona.mk
@@ -180,6 +180,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libgui_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
