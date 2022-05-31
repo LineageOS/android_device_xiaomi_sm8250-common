c719e5050237dd3b83e27f664385a4d265a4902a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-31 14:34:01 +0200

    sm8250-common: rootdir: Merge all our custom init rc into a single one
    Change-Id: I9e4f9fbcc76986a02288a439706affecde25594f

--- kona.mk
+++ kona.mk
@@ -407,12 +407,11 @@ PRODUCT_PACKAGES += \
     init.qti.dcvs.sh
 
 PRODUCT_PACKAGES += \
-    init.motor.rc \
-    init.nfc.rc \
     init.qcom.power.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.xiaomi.rc \
     fstab.qcom \
     fstab.qcom_ramdisk \
     ueventd.qcom.rc
