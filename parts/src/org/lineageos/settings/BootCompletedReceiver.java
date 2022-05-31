25170bd9bd05e32a9671b8cc9c0dcbf80f5cf789
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-31 16:17:13 +0200

    sm8250-common: parts: Improve popup camera service HIDL detection
    Change-Id: Id19e49429b275ab743e2dc8e484cc67ddc854898

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -35,7 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        PopupCameraUtils.startService(context);
+        PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
     }
 }
