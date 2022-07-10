08e1b0cadc7a6ac4b43be470c8b23e691819f63b
xSylla <syllamodder@gmail.com>
2022-07-11 01:08:31 +0200

    sm8250-common: vibrator: Add support for aw8624_haptic
    Change-Id: Idf66c48cbdd17ede3f73cc5e5c0590225737158b

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -114,7 +114,7 @@ InputFFDevice::InputFFDevice()
         }
 
         if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics") &&
-                strcmp(name, "aw8697_haptic")) {
+                strcmp(name, "aw8697_haptic") && strcmp(name, "aw8624_haptic")) {
             ALOGD("not a qcom/qti haptics device\n");
             close(fd);
             continue;
