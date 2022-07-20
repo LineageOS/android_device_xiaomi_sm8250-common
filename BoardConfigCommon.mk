a35be7e7f405033a1b452c9b9d3d1313b9e920fb
Alexander Koskovich <akoskovich@pm.me>
2022-07-20 09:16:53 -0700

    sm8250-common: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,7 +175,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
