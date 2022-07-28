b6f2280dfc3dc3cfe46dc8d1b6d52bf04d8243f7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-29 00:32:39 +0200

    sm8250-common: Update blobs from munch V13.0.4.0.SLMMIXM
    Change-Id: Ia1945730e3efd9d019b3d4ce048f8aca247c6708

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -795,8 +795,13 @@ function configure_memory_parameters() {
 
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
-    echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    if [[ "$ProductName" == "munch"* ]]; then
+          echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
+          echo 60 > /proc/sys/vm/swappiness
+    else
+          echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
+          echo 100 > /proc/sys/vm/swappiness
+    fi
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
