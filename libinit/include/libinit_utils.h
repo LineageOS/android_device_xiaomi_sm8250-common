9ec1f585c5a3fdcc61724751628aa1577f8a51ea
althafvly <althafvly@gmail.com>
2022-09-12 10:05:25 +0530

    sm8250-common: libinit: Drop overriding description
    - Not used anywhere in stock.
    
    Change-Id: I3c3ae5682b732add3eb36e6f3e3b03269d4cc793

--- libinit/include/libinit_utils.h
+++ libinit/include/libinit_utils.h
@@ -13,6 +13,4 @@ void property_override(std::string prop, std::string value, bool add = true);
 
 void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);
 
-std::string fingerprint_to_description(std::string fingerprint);
-
 #endif // LIBINIT_UTILS_H
