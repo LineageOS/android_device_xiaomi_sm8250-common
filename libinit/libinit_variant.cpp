98f44e1831f6f6ee1312bede6e0b1867904a591e
althafvly <althafvly@gmail.com>
2022-09-20 11:16:46 +0530

    sm8250-common: libinit: Set default USB and Bluetooth name
    Change-Id: I65948d08d95b138d9faed262ad3376305b9510a7

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -33,8 +33,10 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("device", variant.device, true);
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
+    property_override("vendor.usb.product_string", variant.marketname, true);
 
     if (access("/system/bin/recovery", F_OK) != 0) {
+        property_override("bluetooth.device.default_name", variant.marketname, true);
         set_ro_build_prop("fingerprint", variant.build_fingerprint);
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
 
