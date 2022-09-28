55092e9b0690ced422720990212905a8920dbf9c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:43:20 +0200

    sm8250-common: Remove non-existing or implicitly included IPACM packages
    Change-Id: Iae06f09e101b237187638e76a6dd903ecdc2ca4f

--- kona.mk
+++ kona.mk
@@ -274,9 +274,7 @@ PRODUCT_PACKAGES += \
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
