52f3002e25b9cd37d765145e9cf934cabaf6b76a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:38:17 +0200

    sm8250-common: Build default health HIDL recovery impl
    Change-Id: I9710760a1b7895ed9a0c3da967c4b30ea655bf94

--- kona.mk
+++ kona.mk
@@ -251,6 +251,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+ifneq ($(TARGET_IS_VAB),true)
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl.recovery
+endif
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
