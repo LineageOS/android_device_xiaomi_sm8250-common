b8860fb03c36252afa17b0d5ef288a085ca4da22
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-05 01:09:56 +0100

    sm8250-common: Add dagu and psyche to the guards
    Change-Id: Iea47e7dbe2ad902b82aeb3d58dc2c28b528165f0

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "alioth apollon cas cmi elish enuma lmi munch thyme umi"
+write_headers "alioth apollon cas cmi dagu elish enuma lmi munch psyche thyme umi"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
