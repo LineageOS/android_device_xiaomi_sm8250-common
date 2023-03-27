5d50d4b13282ebd1c4ac28995ad0983130c6f4e8
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    sm8250-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- kona.mk
+++ kona.mk
@@ -478,7 +478,7 @@ PRODUCT_COPY_FILES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
