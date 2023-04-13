760ddad313bbdda7357591d712b137aa14634d81
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-14 01:23:39 +0200

    sm8250-common: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- kona.mk
+++ kona.mk
@@ -412,7 +412,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
