53e10b476d2e1ef05d5b11700f82b3d058c836c9
Michael Bestas <mkbestas@lineageos.org>
2023-05-19 00:20:56 +0300

    sm8250-common: Switch to common QTI vibrator HAL
    Change-Id: I281c6d993901ad86592d43afcac2171840998738

--- kona.mk
+++ kona.mk
@@ -470,11 +470,7 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
