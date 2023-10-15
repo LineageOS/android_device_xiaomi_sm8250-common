03fa5f3e5b77f9611f12fa7c016f28319c00224a
Davide Garberi <dade.garberi@gmail.com>
2023-10-16 01:59:38 +0200

    sm8250-common: Exclude unneeded targets from tablet builds
    Change-Id: I921f54a6eec98a390783d45cf9bf228593fe5e78

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,7 @@ if [ -z "${ONLY_TARGET}" ]; then
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files-phone.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
