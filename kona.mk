bfeb2a6ae19948a6e29319ffbdf4d5721e0ae88a
LuK1337 <priv.luk@gmail.com>
2023-10-19 23:08:10 +0200

    sm8250-common: Build AIDL NXP NFC HAL
    Change-Id: I6ace3ac149c84d2cb9e2773e7ef5ddbf24c35b92

--- kona.mk
+++ kona.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc_snxxx@1.2-service \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     libchrome.vendor \
     NfcNci \
