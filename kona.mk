7e4da64f0060de6b33d0bc87f457b8910f80b161
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    sm8250-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- kona.mk
+++ kona.mk
@@ -417,6 +417,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service
 
