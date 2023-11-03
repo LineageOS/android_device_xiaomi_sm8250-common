8d69d1ea86c0072ab1afb812efc0c37f333cf294
Icarus Xu <o0icarusxu0o@gmail.com>
2023-11-03 17:59:20 +0800

    sm8250-common: volume control: add keylayout file for headphone jack
    to control volume by pressing volume +/- button on wired headphone
    
    Change-Id: I8eb0c1aee620334793fadc3d05530e50411c7d3c
    Signed-off-by: Icarus Xu <o0icarusxu0o@gmail.com>

--- kona.mk
+++ kona.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/kona-mtp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/kona-mtp-snd-card_Button_Jack.kl
+
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
