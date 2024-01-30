15e0cfcdc42b556060252d5473737d4d740ff9c9
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    sm8250-common: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,9 +155,6 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
