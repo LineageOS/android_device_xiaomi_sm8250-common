d007ef4c2a619cba39f11d10efaeab993cb1c621
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-29 04:25:24 +0800

    sm8250-common: Make RIL blob read device name from a dedicated property
    * vendor/lib64/libril-qc-hal-qmi.so checks for device name from the
      property `ro.product.vendor.device`, If it gets a wrong device name
      then the modem would end up crashing with CRITICAL_DATA_CHECK_FAILED
      error (perhaps it's Xiaomi-specific thing)
    * `ro.product.vendor.device` property could be spoofed to something
      else by the user sometimes, so let's switch to another property to
      make sure RIL won't get broke because of it
    
    Change-Id: I90c5594d43ba6b590500806c4b4b83b0c9878fea

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,9 @@ function blob_fixup() {
         vendor/etc/media_codecs_kona.xml)
             sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
             ;;
+        vendor/lib64/libril-qc-hal-qmi.so)
+            sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
+            ;;
     esac
 }
 
