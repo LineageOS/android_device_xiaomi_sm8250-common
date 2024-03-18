0f4a9b04bb0f2377f9c959d484f59dccadefdad4
althafvly <althafvly@gmail.com>
2024-03-18 17:36:12 +0530

    Revert "sm8250-common: touch: add IHighTouchPollingRate"
    This reverts commit 7172b20f4f85ef0beb7483620ac63cd1bec44f55.
    
    Reason: Kernel part is not implemented.
    
    Change-Id: I8625e4cf6a1af5ba9febec38736e61a82007f728

--- kona.mk
+++ kona.mk
@@ -467,10 +467,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
