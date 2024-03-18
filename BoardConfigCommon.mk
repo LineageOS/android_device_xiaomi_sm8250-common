2f4a921741dd63f588ef9b1460074e22c64dfbc3
althafvly <althafvly@gmail.com>
2024-03-18 05:54:07 +0530

    sm8250-common: Update blobs from munch V14.0.6.0.TLMMIXM
    Change-Id: I0c41f8226416962d812c4ef936463418bce9b1ff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-06-01
+VENDOR_SECURITY_PATCH := 2024-02-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
