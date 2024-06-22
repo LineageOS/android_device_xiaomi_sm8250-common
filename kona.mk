ed8c893b92d632fec2c961296e906c0657faccd4
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-22 19:24:15 +0200

    sm8250-common: Fix camera library rule location
    Change-Id: I24b30a7e187273fce2e23847debe5bff473bfe23

--- kona.mk
+++ kona.mk
@@ -148,7 +148,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
@@ -156,6 +155,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++_vendor \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
