13c0754561bfc0954123062485ffb285924335f4
luffitys <banackidaniel@gmail.com>
2024-07-05 15:39:56 +0200

    sm8250-common: libinit: Add 8GB dalvik configuration
    Change-Id: Iadcb0a52c581bd71ebf614c644d5639b3fc1161c

--- libinit/libinit_dalvik_heap.cpp
+++ libinit/libinit_dalvik_heap.cpp
@@ -18,6 +18,15 @@
 
 #define GB(b) (b * 1024ull * 1024 * 1024)
 
+static const dalvik_heap_info_t dalvik_heap_info_8192 = {
+    .heapstartsize = "24m",
+    .heapgrowthlimit = "256m",
+    .heapsize = "512m",
+    .heapminfree = "8m",
+    .heapmaxfree = "48m",
+    .heaptargetutilization = "0.46",
+};
+
 static const dalvik_heap_info_t dalvik_heap_info_6144 = {
     .heapstartsize = "16m",
     .heapgrowthlimit = "256m",
@@ -51,7 +60,9 @@ void set_dalvik_heap() {
 
     sysinfo(&sys);
 
-    if (sys.totalram > GB(5))
+    if (sys.totalram > GB(7))
+        dhi = &dalvik_heap_info_8192;
+    else if (sys.totalram > GB(5))
         dhi = &dalvik_heap_info_6144;
     else if (sys.totalram > GB(3))
         dhi = &dalvik_heap_info_4096;
