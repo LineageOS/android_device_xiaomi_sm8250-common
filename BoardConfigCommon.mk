e5a1306eeaa082168a5a8dd04d72787215239d70
luffitys <banackidaniel@gmail.com>
2024-07-05 15:39:04 +0200

    sm8250-common: Switch to dot product CPU variant
     * cmi:/ $ cat /proc/cpuinfo | grep -m 1 Features
       Features: fp asimd evtstrm aes pmull sha1 sha2 crc32
       atomics fphp asimdhp cpuid asimdrdm lrcpc dcpop asimddp
    
     * From this, it's evident that our CPU (Snapdragon 865)
       supports all the features of ARMv8.2-DotProd [1].
       Moreover AOSP sets this arch variant in their generic
       cortex-a55 ART target as well [2]. This should unlock
       some optimizations in different code paths.
    
    [1]: https://en.wikichip.org/wiki/arm/armv8#ARMv8_Extensions_and_Processor_Features
    [2]: https://android.googlesource.com/device/generic/art/+/refs/heads/master/armv8_cortex_a55/BoardConfig.mk#23
    
    Change-Id: Ia6b4cd424d6985f73460484a398c16067f9446b7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,7 +28,7 @@ endif
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
