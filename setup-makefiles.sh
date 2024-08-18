e5edf0710a88dc4375c69e2b1e38d4b4a6623a78
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sm8250-common: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,6 +48,10 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.imsrtpservice@3.0)
             echo "${1}_vendor"
             ;;
+        libOmxCore | \
+            libgrallocutils | \
+            libwfdaac_vendor | \
+            libwpa_client) ;;
         *)
             return 1
             ;;
