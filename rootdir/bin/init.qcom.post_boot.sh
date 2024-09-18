a59442f34505cf46dd92167bc2e11718be92d0e8
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-18 22:18:27 +0800

    sm8250-common: rootdir: Fix derps in `configure_memory_parameters()`
    * Remove product name guards where we need
    * Don't call same functions twice
    * Take munch's swappiness
    
    Change-Id: I48e5ed36f151f0f73fb82291a6cbcbc11f93b52a

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -954,12 +954,8 @@ function configure_memory_parameters() {
     ProductName=`getprop ro.product.name`
     low_ram=`getprop ro.config.low_ram`
 
-    if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$ProductName" == "sdmshrike_au" ] || [ "$ProductName" == "alioth" ]; then
-        # Enable ZRAM
-        configure_zram_parameters
-        configure_read_ahead_kb_values
+    if true; then
         echo 0 > /proc/sys/vm/page-cluster
-        echo 100 > /proc/sys/vm/swappiness
 
         #add memory limit to camera cgroup
         MemTotalStr=`cat /proc/meminfo | grep MemTotal`
@@ -1075,14 +1071,9 @@ function configure_memory_parameters() {
     fi
 
     # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
-    if [[ "$ProductName" == "munch"* ]]; then
-          echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-          echo 60 > /proc/sys/vm/swappiness
-    else
-          echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-          echo 100 > /proc/sys/vm/swappiness
-    fi
+    # Set swappiness to 60 for all targets
+    echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
+    echo 60 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
